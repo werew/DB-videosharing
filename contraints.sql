89f80bc8b3cdd4e4d26231d83e853ea452b94742
CONIGLIO Luigi <coniglio@osr-appli1.u-strasbg.fr>
2016-10-30 22:05:04 +0100

    Exo 2

--- contraints.sql
+++ contraints.sql
@@ -45,3 +45,49 @@ show errors;
 
 
 
+/* Exercice 2 */
+
+/* Met a jour toutes les dates de disponibilite a chaque UPDATE/INSERT */
+CREATE OR REPLACE TRIGGER UpdateExpiration
+AFTER INSERT OR UPDATE ON Diffusion
+DECLARE
+	lastdiff_v 	Diffusion.Time%TYPE;
+BEGIN
+	FOR my_row IN (SELECT VideoID, MAX(Time) LastDiff
+		       FROM Diffusion GROUP BY VideoID)
+	LOOP
+		UPDATE Video v SET v.Expiration = my_row.LastDiff + 14
+		WHERE v.VideoID = my_row.VideoID;
+	END LOOP;
+END;
+/
+
+
+show errors;
+
+
+/* Met a jour chaque les dates de disponibilite par rapport
+   a les diffusions ajoute/mises jour. 
+ */
+
+/*
+CREATE OR REPLACE TRIGGER UpdateExpiration
+AFTER INSERT OR UPDATE ON Diffusion
+FOR EACH ROW
+DECLARE
+	expiration_v    Video.Expiration%TYPE;
+BEGIN
+	SELECT Expiration INTO expiration_v 
+	FROM Video WHERE VideoID = :new.VideoID;
+
+	IF expiration_v < :new.Time + 14 OR expiration_v IS NULL THEN
+		UPDATE Video SET Video.Expiration = :new.Time + 14
+		WHERE  VideoID = :new.VideoID;
+	END IF;
+END;
+/
+
+show errors; 
+
+
+*/
